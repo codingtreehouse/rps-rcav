f66b80a79c46d82e5c708768c60da3d412ec6dd3
Andrew Johnson <andrewj@chicagobooth.edu>
2023-04-11 01:52:59 +0000

    Finalized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
@@ -9,7 +9,6 @@ class ApplicationController < ActionController::Base
 
   def play_paper
     @comp_move = ["rock", "paper","scissors"].sample
-
     if @comp_move == "rock"
       @outcome = "won"
     elsif @comp_move == "paper"
@@ -17,12 +16,33 @@ class ApplicationController < ActionController::Base
     elsif @comp_move == "scissors"
       @outcome = "lost"
     end
-
-
     render({ :template => "game_templates/user_paper.html.erb"})
   end
 
+  def play_scissors
+    @comp_move = ["rock", "paper","scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    end
+    render({ :template => "game_templates/user_scissors.html.erb"})
+  end
+
+
   def play_rock
+    @comp_move = ["rock", "paper","scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    elsif @comp_move == "scissors"
+      @outcome = "won"
+    end
     render({:template => "game_templates/user_rock.html.erb"})
   end
 end
